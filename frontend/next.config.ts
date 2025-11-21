d7798bd84c445a057c23cee4d0edf5935fc5f185
Lavon Sykes <lavonsykes2003@gmail.com>
2025-11-21 12:12:41 -0500

    base path reconfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', 
   reactStrictMode: true,
-  basePath: "/movinOnUp",
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
 };
 
